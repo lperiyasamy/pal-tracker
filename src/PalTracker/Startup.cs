9099067e34e93c0f720dffe8762df2426208c2dc
Periyasamy L <periyasamy.la@gmail.com>
2020-01-27 16:23:05 +0530

    commiting for multiple environments

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,11 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(sp => new WelcomeMessage(Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE is not configured.")));
+            services.AddSingleton(sp => new CloudFoundryInfo(Configuration.GetValue<string>("PORT", "PORT is not configured."),
+                                                            Configuration.GetValue<string>("MEMORY_LIMIT", "MEMORY_LIMIT is not configured."),
+                                                             Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX is not configured."),
+                                                             Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR is not configured.")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
