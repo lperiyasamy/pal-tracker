d4094768799a2137c1eb9bfd88016a9abf7b8a00
Periyasamy L <periyasamy.la@gmail.com>
2020-01-28 14:36:34 +0530

    pushing DB interaction changes

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -31,7 +32,9 @@ namespace PalTracker
                                                             Configuration.GetValue<string>("MEMORY_LIMIT", "MEMORY_LIMIT is not configured."),
                                                              Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX is not configured."),
                                                              Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR is not configured.")));
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
