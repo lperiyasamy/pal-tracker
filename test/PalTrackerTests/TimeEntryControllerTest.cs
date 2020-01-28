702aa4468d74f55369434c66a0947ecc1e5301fc
Periyasamy L <periyasamy.la@gmail.com>
2020-01-28 16:44:06 +0530

    health monitoring

--- test/PalTrackerTests/TimeEntryControllerTest.cs
+++ test/PalTrackerTests/TimeEntryControllerTest.cs
@@ -11,11 +11,14 @@ namespace PalTrackerTests
     {
         private readonly TimeEntryController _controller;
         private readonly Mock<ITimeEntryRepository> _repository;
+        private readonly Mock<IOperationCounter<TimeEntry>> _operationCounter;
 
         public TimeEntryControllerTest()
         {
             _repository = new Mock<ITimeEntryRepository>();
-            _controller = new TimeEntryController(_repository.Object);
+            _operationCounter = new Mock<IOperationCounter<TimeEntry>>();
+            _controller = new TimeEntryController(_repository.Object,_operationCounter.Object);
+             _operationCounter.Setup(oc => oc.Increment(It.IsAny<TrackedOperation>()));
         }
 
         [Fact]
@@ -33,6 +36,7 @@ namespace PalTrackerTests
 
             Assert.Equal(expected, typedResponse.Value);
             Assert.Equal(200, typedResponse.StatusCode);
+            _operationCounter.Verify(oc => oc.Increment(TrackedOperation.Read), Times.Once);
         }
 
         [Fact]
